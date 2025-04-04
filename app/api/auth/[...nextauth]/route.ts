265fe807175e11fc1a669c282b963f87bdc50c5f
hmad <f23barin1e02112@iub.edu.pk>
2025-04-05 00:01:58 +0500

    auth fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,8 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import GithubProvider from "next-auth/providers/github";
-import prisma from "@/lib/prisma";
-import type { Session, User } from "next-auth";
+import { authOptions } from "@/lib/auth-options";
+import NextAuth from "next-auth";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    session({ session, user }: { session: Session; user: User }) {
-      if (session.user && user?.id) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
-};
+
+
 
 const handler = NextAuth(authOptions);
 
